47cc6a92f931e5bce77e2fd3d84f9f0081415279
mahii47 <mahmankar333@gmail.com>
2025-09-08 23:29:16 +0530

    Headless browser is added.

--- CuraHealthcareProject/src/main/java/commonLibs/BaseTest.java
+++ CuraHealthcareProject/src/main/java/commonLibs/BaseTest.java
@@ -1,6 +1,7 @@
 package commonLibs;
 import java.sql.ResultSet;
 import java.time.Duration;
+import java.util.ArrayList;
 import java.util.List;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -9,6 +10,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import drivers.DriverFactory;
 
 public class BaseTest implements IBrowserActions {
@@ -135,4 +137,6 @@ public class BaseTest implements IBrowserActions {
 		Assert.assertEquals("https://katalon-demo-cura.herokuapp.com/appointment.php#summary", url);
 		sleep();
 	}	
+	
+	
 }
